368a3202efa85f071f89e25099dc2f97c396d56e
nikolasalim <nikolasalim@gmail.com>
2020-03-09 10:57:13 +0100

    set up database

---
+++ db.js
@@ -0,0 +1,10 @@
+const Sequelize = require("sequelize");
+const baseUrl =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:secret@localhost:5432/postgres";
+
+const db = new Sequelize(baseUrl);
+
+db.sync()
+  .then(() => console.log("Database is synced!"))
+  .catch(error => console.error(error));
